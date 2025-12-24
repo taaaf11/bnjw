bd0217b4f620936ef79f82e3a3220c72f1f369d2
taaaf11 <taafuuu@gmail.com>
2025-12-24 22:07:35 +0500

    p3 - 20

---
+++ P3 -- Cracking/20.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+struct node {
+    char value;
+    node *next;
+
+    node(char v) {
+        value = v;
+        next = NULL;
+    }
+};
+
+
+void insertNode(node **head, char v) {
+    node *newnode = new node(v);
+    if (*head == NULL) {
+        *head = newnode;
+        return;
+    }
+
+    else {
+        node *current = *head;
+        while (current->next) {
+            current = current->next;
+        }
+        current->next = newnode;
+    }
+}
+
+
+node* detectCycle(node *head) {
+    unordered_set<char> visited;
+
+    while (head) {
+        if (visited.find(head->value) != visited.end()) {
+            return head;
+        }
+
+        visited.insert(head->value);
+        head = head->next;
+    }
+
+    return NULL;
+}
+
+int main() {
+    node *head = NULL;
+
+    insertNode(&head, 'a');
+    insertNode(&head, 'b');
+    insertNode(&head, 'c');
+    insertNode(&head, 'd');
+    insertNode(&head, 'e');
+
+    head->next->next->next->next->next = head->next->next;
+
+    node *cycleDetect = detectCycle(head);
+
+    if (cycleDetect) {
+        cout << "Cycle detected at: " << cycleDetect->value << endl;
+    }
+    else {
+        cout << "No cycles." << endl;
+    }
+
+    return 0;
+}
