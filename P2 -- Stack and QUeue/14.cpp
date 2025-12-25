721be4ed8c90f7aa2ada9cc4e517f903fdf8132c
taaaf11 <taafuuu@gmail.com>
2025-12-25 12:31:00 +0500

    modify p2 - 14

--- P2 -- Stack and QUeue/14.cpp
+++ P2 -- Stack and QUeue/14.cpp
@@ -6,84 +6,10 @@
 using namespace std;
 
 
-// struct MyQueue {
-//     vector<int> v;
-//     int front, rear, totalSize;
-//     // int totalSize;
-
-//     MyQueue() {
-//         front = rear = -1;
-//         v.resize(100);
-//     }
-
-//     MyQueue(int totalSize) {
-//         front = rear = -1;
-//         this->totalSize = totalSize;
-//         v.resize(totalSize);
-//     }
-
-//     MyQueue(initializer_list<int> l) {
-//         front = 0;
-//         v.assign(l);
-//         totalSize = l.size();
-//     }
-
-//     void enqueue(int value) {
-//         rear++;
-//         v[rear] = value;
-//         if (front == -1) {
-//             front = 0;
-//         }
-//         totalSize++;
-//     }
-
-//     int dequeue() {
-//         if (front == totalSize) {
-//             cout << "Queue empty" << endl;
-//             exit(1);
-//         }
-//         int elem = v[front];
-//         front++;
-//         return elem;
-//     }
-
-//     bool empty() {
-//         return (front == totalSize);
-//     }
-// };
-
-
-// struct TrainBogies {
-//     MyQueue q1;
-    
-//     TrainBogies(initializer_list<int> l) {
-//         for (int i: l) {
-//             q1.enqueue(i);
-//         }
-//         // q1 = MyQueue(l);
-//     }
-
-//     void dequeue() {
-//         MyQueue q2(q1.totalSize);
-//         stack<int> s;
-
-//         int &v = q1.dequeue();
-//         q2.enqueue(v);
-//         if (!s.empty()) {
-//             if (v < s.top()) {
-//                 s.push(v);
-//             }
-//         }
-//         else {
-//             s.push(v);
-//         }
-//     }
-// };
-
-struct TrainBogies2 {
+struct TrainBogies {
     queue<int> q1;
 
-    TrainBogies2(initializer_list<int> l) {
+    TrainBogies(initializer_list<int> l) {
         for (int i: l) {
             q1.push(i);
         }
@@ -142,7 +68,7 @@ struct TrainBogies2 {
 };
 
 int main() {
-    TrainBogies2 t1{9, 5, 6, 1, 3};
+    TrainBogies t1{9, 5, 6, 1, 3};
 
     while (true) {
         cout << t1.dequeue() << endl;
