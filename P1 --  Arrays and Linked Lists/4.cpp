95f05a08a5b158244178f52b9103da985463b5cd
taaaf11 <taafuuu@gmail.com>
2025-12-25 08:48:49 +0500

    p1 - 4

---
+++ P1 --  Arrays and Linked Lists/4.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+#include <utility>
+#include <stack>
+using namespace std;
+
+struct node {
+    int value;
+    node *next;
+
+    node(int v) {
+        value = v;
+        next = NULL;
+    }
+};
+
+
+int getSize(node *head) {
+    int size = 0;
+    while (head) {
+        size++;
+        head = head->next;
+    }
+
+    return size;
+}
+
+pair<node *, node *> midsplit(node *head, int size) {
+    // int sizeOfList = ;
+    node *current = head;
+    node *secondPart = NULL;
+    for (int i = 1; i <= ((size - 1) / 2); i++) {
+        // if (i == 0) {
+        //     current = head;
+        //     continue;
+        // }
+        cout << "Adding " << current->value << endl;
+        current = current->next;
+    }
+    secondPart = current->next;
+    current->next = NULL;
+    
+    return {head, secondPart};
+}
+
+
+void insertNode(node **head, int v) {
+    node *newnode = new node(v);
+    if (*head == NULL) {
+        *head = newnode;
+        return;
+    }
+
+    else {
+        node *current = *head;
+        while (current->next) {
+            current = current->next;
+        }
+        current->next = newnode;
+    }
+}
+
+void reverse(node *head) {
+    node *current = head;
+    stack<int> s;
+    while (current) {
+        s.push(current->value);
+        current = current->next;
+    }
+    current = head;
+    while (!s.empty()) {
+        current->value = s.top();
+        s.pop();
+        current = current->next;
+    }
+}
+
+bool compareLists(node *one, node *two) {
+    while (one && two) {
+        if (one->value != two->value) {
+            return false;
+        }
+        one = one->next;
+        two = two->next;
+    }
+
+    if (one || two) {
+        return false;
+    }
+
+    return true;
+}
+
+
+void printList(node *head) {
+    while (head) {
+        cout << head->value << " -> ";
+        head=head->next;
+    }
+}
+
+int main() {
+    node *head = NULL;
+    int size = 5;
+
+    insertNode(&head, 1);
+    insertNode(&head, 3);
+    insertNode(&head, 7);
+    insertNode(&head, 3);
+    insertNode(&head, 1);
+    
+    printList(head);
+
+    pair<node *, node *> splitted = midsplit(head, size);
+    node *firstPart = splitted.first;
+    node *secondPart = splitted.second;
+
+    node *secondStart = NULL;
+
+    if (size % 2 != 0) {
+        node *current = firstPart;
+        while (current && current->next) {
+            current = current->next;
+        }
+
+        secondStart = new node(current->value);
+        secondStart->next = secondPart;
+        secondPart = secondStart;
+    }
+
+    reverse(secondPart);
+
+
+
+
+    // if (secondStart != NULL) {
+    //     secondPart = secondStart;
+    // }
+
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    printList(firstPart);
+    cout << endl;
+    printList(secondPart);
+    cout << endl;
+    cout << compareLists(firstPart, secondPart) << endl;
+
+    return 0;
+}
