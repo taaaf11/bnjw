d27cc986648f06bf857e3305a80959aec75f9660
taaaf11 <taafuuu@gmail.com>
2025-12-27 14:52:34 +0500

    comment out cases with no implementation

--- main.cpp
+++ main.cpp
@@ -10,15 +10,15 @@ int main() {
         system("cls");
 
         cout << "1. Show P1 - 1" << endl;
-        cout << "2. Show P1 - 2" << endl;
+        // cout << "2. Show P1 - 2" << endl;
         cout << "3. Show P1 - 3" << endl;
         cout << "4. Show P1 - 4" << endl;
         cout << "5. Show P1 - 5" << endl;
         cout << "6. Show P1 - 6" << endl;
         cout << "7. Show P1 - 7" << endl;
-        cout << "8. Show P1 - 8" << endl;
+        // cout << "8. Show P1 - 8" << endl;
         cout << "9. Show P1 - 9" << endl;
-        cout << "10. Show P1 - 10" << endl;
+        // cout << "10. Show P1 - 10" << endl;
         cout << "11. Show P2 - 11" << endl;
         cout << "12. Show P2 - 12" << endl;
         cout << "13. Show P2 - 13" << endl;
@@ -29,15 +29,13 @@ int main() {
         cout << "18. Show P3 - 18" << endl;
         cout << "19. Show P3 - 19" << endl;
         cout << "20. Show P3 - 20" << endl;
+        cout << "00. Quit" << endl;
         cout << ">>> ";
         cin >> choice;
 
-        switch (choice) {
+        switch ((char) choice) {
             case 1:
                 showP1();
-                break;
-            case 2:
-                // showP2();
                 break;
             case 3:
                 showP3();
@@ -54,14 +52,8 @@ int main() {
             case 7:
                 showP7();
                 break;
-            case 8:
-                // showP8();
-                break;
             case 9:
                 showP9();
-                break;
-            case 10:
-                // showP10();
                 break;
             case 11:
                 showP11();
@@ -93,14 +85,14 @@ int main() {
             case 20:
                 showP20();
                 break;
+            case 0:
+                return 0;
+            default:
+                cout << "Not implemented." << endl;
         }
 
         cout << endl << endl << "Press any key to continue...";
-        int c = getch();
-        if (c == 'q' || c == 'Q') {
-            break;
-        }
-
+        getch();
     }
     return 0;
 }
